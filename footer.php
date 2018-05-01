2cb82d63daddd0e0e2d225048487843975c0ae3a
M4ko <31808514+M4ko1339@users.noreply.github.com>
2018-05-01 04:19:18 +0200

    Started working on new design.

--- footer.php
+++ footer.php
@@ -1,2 +1,5 @@
+
+<!-- JS -->
+<script type="text/javascript" src="js/materialize.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -15,7 +15,7 @@ include('inc/functions.php');
     <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
 
     <!-- CSS Stylesheets -->
-    <link rel="stylesheet" type="text/css" href="css/materialize.css" media="screen,projection">
+    <link rel="stylesheet" type="text/css" href="css/materialize.min.css" media="screen,projection">
     <link rel="stylesheet" type="text/css" href="css/main.css" media="screen,projection">
     <link rel="stylesheet" type="text/css" href="css/content.css" media="screen,projection">
     <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css" media="screen,projection">
@@ -24,17 +24,27 @@ include('inc/functions.php');
 <div class="topline">
     <div class="container">
         <div class="row">
-            <div class="logo col s12">
+            <div class="logo col m4">
                 Core<span class="green-text">CMS</span>
             </div>
 
-            <div class="menu col s12">
+            <div class="menu col m8">
                 <ul>
-                    <li><a href="#" class="current-nav">Dashboard</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Users</a></li>
-                    <li><a href="#" class="logout">Logout</a></li>
+                    <li><a href="#" class="current-nav"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+                    <li><a href="#"><i class="far fa-newspaper"></i> News</a></li>
+                    <li><a href="#"><i class="fas fa-users"></i> Users</a></li>
+                    <li><a href="#" class="logout"><i class="fas fa-power-off"></i> Logout</a></li>
                 </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="notifications col s12">
+            <div class="notification red">
+                Important message to staff
             </div>
         </div>
     </div>
