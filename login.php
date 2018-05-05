7bb4ce836faf3ad3e4c75a25b93284140b815cd7
M4ko1339 <m4ko1339@gmail.com>
2018-05-05 02:59:51 +0200

    Started writing the login page and added stylesheet login.css

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+include('inc/db.php');
+include('inc/functions.php');
+
+$user = new User();
+
+?>
+<html>
+<head>
+    <title>CoreCMS - Login</title>
+    <meta http-equiv="content-type" charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+
+    <!-- CSS Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="css/materialize.min.css" media="screen,projection">
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="screen,projection">
+    <link rel="stylesheet" type="text/css" href="css/login.css" media="screen,projection">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css" media="screen,projection">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+
+    <!-- JS -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+</body>
+</html>
