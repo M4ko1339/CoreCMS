ecd668d785bb2278ea4cf50764c1a038cd6660cd
M4ko <31808514+M4ko1339@users.noreply.github.com>
2018-05-11 02:29:20 +0200

    Started work on dashboard page.

--- index.php
+++ index.php
@@ -8,72 +8,61 @@ include('header.php');
     <div class="row">
         <div class="content col s12">
             <div class="top-menu">
-                <a href="#" class="btn">New Post</a>
+                <a href="#" class="btn">New Notification</a>
             </div>
         </div>
 
-        <div class="content col s12 m6">
-            <div class="content-header col s12">
-                Custom Box
-            </div>
+        <div class="content col s12 m9">
+            <table>
+                <th>Color</th>
+                <th>Message</th>
+                <th>Status</th>
+                <th></th>
+                <th></th>
+                <th class="right"></th>
 
-            <div class="content-box col s12">
-                <div class="input-field col s12">
-                    <label>Text Input</label>
-                    <input type="text" />
-                </div>
-
-                <div class="input-field col s12">
-                    <label>Password Input</label>
-                    <input type="password" />
-                </div>
-
-                <div class="input-field col s12">
-                    <input type="submit" class="btn" value="Button" />
-                </div>
-            </div>
-        </div>
-
-        <div class="content col s12 m6">
-            <table class="responsive-table">
-                <th>Name</th>
-                <th>Age</th>
-                <th>Country</th>
-
-                <tr>
-                    <td>John</td>
-                    <td>42</td>
-                    <td>USA</td>
-                </tr>
-
-                <tr>
-                    <td>Ola</td>
-                    <td>24</td>
-                    <td>Norway</td>
-                </tr>
-
-                <tr>
-                    <td>Justin</td>
-                    <td>24</td>
-                    <td>Netherlands</td>
-                </tr>
+                <?php foreach($notify->Show() as $row): ?>
+                    <tr>
+                        <td>Green</td>
+                        <td>Important message to the staff!</td>
+                        <td class="green-text">Active</td>
+                        <td><a href="#"><i class="fas fa-toggle-on orange-text"></i></a></td>
+                        <td><a href="?edit=<?php echo $row['id']; ?>"><i class="far fa-edit green-text"></i></a></td>
+                        <td class="right"><a href="?delid=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure?');"><i class="fas fa-trash red-text"></i></a></td>
+                    </tr>
+                <?php endforeach; ?>
             </table>
         </div>
 
-        <div class="content col s12 m6">
-            <div class="content-header col s12">
-                Custom Box
-            </div>
-
+        <div class="content col s12 m3">
             <div class="content-box col s12">
-                <form>
-                    <p>
-                        <label>
-                            <input type="checkbox" class="filled-in" checked="checked" />
-                            <span>Permission #1</span>
-                        </label>
-                    </p>
-                </form>
+                <div class="info-box">
+                    <label>Username</label>
+                    <div class="info-box-text">
+                        Admin
+                    </div>
+                </div>
+
+                <div class="info-box">
+                    <label>Email</label>
+                    <div class="info-box-text">
+                        admin@corecms.com
+                    </div>
+                </div>
+
+                <div class="info-box">
+                    <label>Last IP</label>
+                    <div class="info-box-text">
+                        127.0.0.1
+                    </div>
+                </div>
+
+                <div class="info-box">
+                    <label>Last Login</label>
+                    <div class="info-box-text">
+                        10:20 - 11.05.2018
+                    </div>
+                </div>
             </div>
         </div>
     </div>
