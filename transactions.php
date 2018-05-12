a782ea7445db6ae7d6b99809ba58dd2e0375163b
M4ko <31808514+M4ko1339@users.noreply.github.com>
2018-05-12 03:09:29 +0200

    Added transaction page.

---
+++ transactions.php
@@ -0,0 +1,39 @@
+<?php
+
+include('header.php');
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="content col s12">
+            <div class="top-menu">
+                
+            </div>
+        </div>
+
+        <div class="content col s12">
+            <table>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php
+
+include('footer.php');
+
+?>
